58a2a5157da959c6c6583250eb4c358180c76b7c
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-09-20 23:42:42 -0400

    Nav Bar fase 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import logo from './../img/pizzaKitchen-logo.svg'
 
 const HeaderMain = styled.header`
 	box-shadow: 0 2px 20px 0 rgba(149, 158, 172, 0.3);
@@ -8,7 +9,16 @@ const HeaderMain = styled.header`
 const Nav = styled.nav`
 	display: flex;
 	justify-content: space-between;
+	align-items: center;
 	padding: 1.5rem;
+	font-size: 1.6rem;
+	font-weight: 500;
+`
+
+const Image = styled.img`
+	width: 5rem;
+	height: auto;
+	margin-right: 1rem;
 `
 
 const Ul = styled.ul`
@@ -17,18 +27,33 @@ const Ul = styled.ul`
 	list-style: none;
 `
 
+const Li = styled.li`
+	margin-right: 1.8rem;
+`
+
+const Container = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`
+
 const Header = () => {
 	return (
 		<>
 			<HeaderMain>
 				<Nav>
-					<img src='logo' alt='' />
-					<Ul>
-						<li>CONTACT</li>
-						<li>ABOUT US </li>
-						<li>SIGN UP / LOGIN</li>
-					</Ul>
-					<li>LIGTH/DARK</li>
+					<Container>
+						<Image src={logo} alt='logo' />
+						<p>Joe's Pizza</p>
+					</Container>
+					<Container>
+						<Ul>
+							<Li>CONTACT</Li>
+							<Li>ABOUT US </Li>
+							<Li>SIGN UP/LOGIN</Li>
+						</Ul>
+						<li>LIGTH/DARK</li>
+					</Container>
 				</Nav>
 			</HeaderMain>
 		</>
